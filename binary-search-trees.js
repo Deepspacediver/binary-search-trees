a91d9038d16fdbdde325d774dfd983ce9faa9377
deepspacediver <luka.koscik@gmail.com>
2022-11-07 09:14:44 +0100

    added insert method

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -16,6 +16,7 @@ class Tree {
     this.array = mergeSort(this.array);
     return this;
   }
+
   removeDuplicates(arr = this.array) {
     arr.forEach((item) => {
       let indexOfItem = arr.findIndex((element) => element === item);
@@ -24,32 +25,31 @@ class Tree {
     });
     return this;
   }
+
   buildTree(arr = this.array, startArr = 0, endArr = arr.length - 1) {
-    console.log({arr})
     if (startArr > endArr) return null;
-    else {
-      const midArr = Math.floor((startArr + endArr) / 2);
-      const node = new Node(arr[midArr])
-      console.log(node)
-      node.left = this.buildTree(arr.slice(startArr, midArr))
-      node.right = this.buildTree(arr.slice(midArr+1))
-      this.root = node
-      return node
-    }
+
+    const midArr = Math.floor((startArr + endArr) / 2);
+    const node = new Node(arr[midArr]);
+    node.left = this.buildTree(arr.slice(startArr, midArr));
+    node.right = this.buildTree(arr.slice(midArr + 1));
+    this.root = node;
+    return node;
+  }
+
+  insert(valueToInsert, treeNode = this.root) {
+    if (treeNode === null) return new Node(valueToInsert);
+    
+    if (valueToInsert < treeNode.value) {
+      treeNode.left = this.insert(valueToInsert, treeNode.left);
+    } else treeNode.right = this.insert(valueToInsert, treeNode.right);
+    console.log(treeNode);
+    return treeNode;
   }
 }
 let dataArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const myTree = new Tree(dataArray);
-myTree.sortArray().removeDuplicates().buildTree()
-console.log(myTree)
-
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
-}
-console.log(prettyPrint(myTree.root)) 
+myTree.sortArray().removeDuplicates().buildTree();
+myTree.insert(0);
+myTree.insert(-1);
+console.log(myTree);
